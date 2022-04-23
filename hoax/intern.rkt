49c49a01875729f9a56d2005ab3eee1577dd451c
yh-ki <84252233+yh-ki@users.noreply.github.com>
2022-04-23 18:11:43 -0400

    Add files via upload

--- hoax/intern.rkt
+++ hoax/intern.rkt
@@ -10,14 +10,14 @@
 
 (define (count-str e l)
   (match e
-    [(Int i) (l)]
-    [(Bool b) (l)]
-    [(Char c) (l)]
-    [(Eof) (l)]
-    [(Empty) (l)]
-    [(Var x) (l)]
+    [(Int i) l]
+    [(Bool b) l]
+    [(Char c) l]
+    [(Eof) l]
+    [(Empty) l]
+    [(Var x) l]
     [(Str s) (if (member s l) l (cons s l))]
-    [(Prim0 p) (l)]
+    [(Prim0 p) l]
     [(Prim1 p e) (count-str e l)]
     [(Prim2 p e1 e2) (count-str e1 (count-str e2 l))]
     [(Prim3 p e1 e2 e3) (count-str e1 (count-str e2 (count-str e3 l)))]
