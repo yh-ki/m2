c7f3e65df47df7085099bb42bdc51c5812cf37b4
yh-ki <84252233+yh-ki@users.noreply.github.com>
2022-04-23 18:41:33 -0400

    Add files via upload

--- hoax/intern.rkt
+++ hoax/intern.rkt
@@ -5,7 +5,7 @@
 ;; Expr -> Expr
 (define (intern e)
   ;; TODO
-  (count-str e '())
+  (replace-str e (count-str e '()))
   )
 
 (define (count-str e l)
@@ -24,5 +24,24 @@
     [(If e1 e2 e3) (count-str e1 (count-str e2 (count-str e3 l)))]
     [(Begin e1 e2) (count-str e1 (count-str e2 l))]
     [(Let x e1 e2) (count-str e1 (count-str e2 l))]
+    )
+  )
+
+(define (replace-str e l)
+  (match e
+    [(Int i) (Int i)]
+    [(Bool b) (Bool b)]
+    [(Char c) (Char c)]
+    [(Eof) (Eof)]
+    [(Empty) (Empty)]
+    [(Var x) (Var x)]
+    [(Str s) (if (member s l) (remove s l) (Var (string->symbol s)))]
+    [(Prim0 p) l]
+    [(Prim1 p e) (replace-str e l)]
+    [(Prim2 p e1 e2) (replace-str e2 (replace-str e1 l))]
+    [(Prim3 p e1 e2 e3) (replace-str e3 (replace-str e2 (replace-str e1 l)))]
+    [(If e1 e2 e3) (replace-str e3 (replace-str e2 (replace-str e1 l)))]
+    [(Begin e1 e2) (replace-str e2 (replace-str e1 l))]
+    [(Let x e1 e2) (replace-str e2 (replace-str e1 l))]
     )
   )
