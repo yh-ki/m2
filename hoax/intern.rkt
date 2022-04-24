c3bd0dc04175f849afaf4c8d440886d1e98e1cec
yh-ki <84252233+yh-ki@users.noreply.github.com>
2022-04-23 20:33:22 -0400

    Add files via upload

--- hoax/intern.rkt
+++ hoax/intern.rkt
@@ -33,36 +33,36 @@
     [(cons s l) (contain-str e s)])
   )
 
-(define (contain-str e l)
-  (match e
-    [(Int i) #f]
-    [(Bool b) #f]
-    [(Char c) #f]
-    [(Eof) #f]
-    [(Empty) #f]
-    [(Var x) #f]
-    [(Str s) (if (eq? s l) (Let (string-symbol s) (Str s) (Var (string-symbol s))) #f)]
-    [(Prim0 p) #f]
-    [(Prim1 p e)
-     (let ((x (contain-str e l)))
-       (if x (Prim1 p x) (Prim1 p e)))]
-    [(Prim2 p e1 e2)
-     (let ((x (contain-str e1 l)))
-       (if x (Prim2 p x e2) (Prim2 p e1 (contain-str e2 l))))]
-    [(Prim3 p e1 e2 e3)
-     (let ((x (contain-str e1 l)))
-       (if x (Prim3 p x e2 e3)
-           (let ((y (contain-str e2 l)))
-             (if y (Prim3 p e1 y e3) (Prim3 p e1 e2 (contain-str e3 l))))))]
-    [(If e1 e2 e3)
-     (let ((x (contain-str e1 l)))
-       (if x (If x e2 e3)
-           (let ((y (contain-str e2 l)))
-             (if y (If e1 y e3) (If e1 e2 (contain-str e3 l))))))]
-    [(Begin e1 e2)
-     (let ((x (contain-str e1 l)))
-       (if x (Begin x e2) (Begin e1 (contain-str e2 l))))]
-    [(Let x e1 e2)
-     (let ((y (contain-str e1 l)))
-       (if y (Let x y e2) (Let x e1 (contain-str e2 l))))]
-  ))
+;(define (contain-str e l)
+;  (match e
+;    [(Int i) #f]
+;    [(Bool b) #f]
+;    [(Char c) #f]
+;    [(Eof) #f]
+;    [(Empty) #f]
+;    [(Var x) #f]
+;    [(Str s) (if (eq? s l) (Let (string-symbol s) (Str s) (Var (string-symbol s))) #f)]
+;    [(Prim0 p) #f]
+;    [(Prim1 p e)
+;     (let ((x (contain-str e l)))
+;       (if x (Prim1 p x) (Prim1 p e)))]
+;    [(Prim2 p e1 e2)
+;     (let ((x (contain-str e1 l)))
+;       (if x (Prim2 p x e2) (Prim2 p e1 (contain-str e2 l))))]
+;    [(Prim3 p e1 e2 e3)
+;     (let ((x (contain-str e1 l)))
+;       (if x (Prim3 p x e2 e3)
+;           (let ((y (contain-str e2 l)))
+;             (if y (Prim3 p e1 y e3) (Prim3 p e1 e2 (contain-str e3 l))))))]
+;    [(If e1 e2 e3)
+;     (let ((x (contain-str e1 l)))
+;       (if x (If x e2 e3)
+;           (let ((y (contain-str e2 l)))
+;             (if y (If e1 y e3) (If e1 e2 (contain-str e3 l))))))]
+;    [(Begin e1 e2)
+;     (let ((x (contain-str e1 l)))
+;       (if x (Begin x e2) (Begin e1 (contain-str e2 l))))]
+;    [(Let x e1 e2)
+;     (let ((y (contain-str e1 l)))
+;       (if y (Let x y e2) (Let x e1 (contain-str e2 l))))]
+;  ))
