899703d22d09f94d8fc30d0326bf70a2f2f7dbdd
yh-ki <84252233+yh-ki@users.noreply.github.com>
2022-04-23 20:30:29 -0400

    Add files via upload

--- hoax/intern.rkt
+++ hoax/intern.rkt
@@ -5,7 +5,7 @@
 ;; Expr -> Expr
 (define (intern e)
   ;; TODO
-  (replace-str e (count-str e '()))
+  e
   )
 
 (define (count-str e l)
@@ -28,20 +28,41 @@
   )
 
 (define (replace-str e l)
-  (match e
-    [(Int i) (Int i)]
-    [(Bool b) (Bool b)]
-    [(Char c) (Char c)]
-    [(Eof) (Eof)]
-    [(Empty) (Empty)]
-    [(Var x) (Var x)]
-    [(Str s) (if (member s l) (remove s l) (Var (string->symbol s)))]
-    [(Prim0 p) l]
-    [(Prim1 p e) (replace-str e l)]
-    [(Prim2 p e1 e2) (replace-str e2 (replace-str e1 l))]
-    [(Prim3 p e1 e2 e3) (replace-str e3 (replace-str e2 (replace-str e1 l)))]
-    [(If e1 e2 e3) (replace-str e3 (replace-str e2 (replace-str e1 l)))]
-    [(Begin e1 e2) (replace-str e2 (replace-str e1 l))]
-    [(Let x e1 e2) (replace-str e2 (replace-str e1 l))]
-    )
+  (match l
+    ['() e]
+    [(cons s l) (contain-str e s)])
   )
+
+(define (contain-str e l)
+  (match e
+    [(Int i) #f]
+    [(Bool b) #f]
+    [(Char c) #f]
+    [(Eof) #f]
+    [(Empty) #f]
+    [(Var x) #f]
+    [(Str s) (if (eq? s l) (Let (string-symbol s) (Str s) (Var (string-symbol s))) #f)]
+    [(Prim0 p) #f]
+    [(Prim1 p e)
+     (let ((x (contain-str e l)))
+       (if x (Prim1 p x) (Prim1 p e)))]
+    [(Prim2 p e1 e2)
+     (let ((x (contain-str e1 l)))
+       (if x (Prim2 p x e2) (Prim2 p e1 (contain-str e2 l))))]
+    [(Prim3 p e1 e2 e3)
+     (let ((x (contain-str e1 l)))
+       (if x (Prim3 p x e2 e3)
+           (let ((y (contain-str e2 l)))
+             (if y (Prim3 p e1 y e3) (Prim3 p e1 e2 (contain-str e3 l))))))]
+    [(If e1 e2 e3)
+     (let ((x (contain-str e1 l)))
+       (if x (If x e2 e3)
+           (let ((y (contain-str e2 l)))
+             (if y (If e1 y e3) (If e1 e2 (contain-str e3 l))))))]
+    [(Begin e1 e2)
+     (let ((x (contain-str e1 l)))
+       (if x (Begin x e2) (Begin e1 (contain-str e2 l))))]
+    [(Let x e1 e2)
+     (let ((y (contain-str e1 l)))
+       (if y (Let x y e2) (Let x e1 (contain-str e2 l))))]
+  ))
